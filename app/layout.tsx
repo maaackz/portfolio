226419c75b287c79b2b55c40d66c4a0c7acd094d
maaackz <maaaaackz@gmail.com>
2025-07-02 15:25:10 +0800

    google site verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,6 +65,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=5" />
+        <meta name="google-site-verification" content="wE-3XvMCAShivHuNRFzGKIfudryPdRYfahPOc1xp5mk" />
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="manifest" href="/manifest.json" />
